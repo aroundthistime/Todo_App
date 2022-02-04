9b7f66dcdcfe0277d7601f0a40887b86a5b6cae9
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2022-02-04 15:35:30 +0900

    todos 관련 module 작성 완료

--- src/modules/root.ts
+++ src/modules/root.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from '@reduxjs/toolkit';
 import quotePrevention from './quotePrevention';
+import todos from './todos';
 
 const rootReducer = combineReducers({
   quotePrevention,
+  todos,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/modules/todos.ts
@@ -0,0 +1,73 @@
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
+
+export type ImportanceLevel = 'high' | 'medium' | 'low';
+
+interface Todo {
+  id: number;
+  title: string;
+  content: string;
+  cleared: boolean;
+  deadline: string;
+  importanceLevel: ImportanceLevel;
+}
+
+type TodosState = Todo[];
+
+const initialState: TodosState = [];
+
+export const clearTodo = createAsyncThunk(
+  'todos/clearTodo',
+  (todoId: number, thunkAPI) => {
+    return {
+      dispatch: thunkAPI.dispatch,
+      updatedFields: {
+        id: todoId,
+        cleared: true,
+      },
+    };
+  },
+);
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo(state, action) {
+      const lastTodoId: number = state[state.length - 1]?.id || 0;
+      state.push({
+        id: lastTodoId + 1,
+        ...action.payload,
+      });
+    },
+    removeTodoById(state, action: PayloadAction<number>) {
+      const index = state.findIndex((todo: Todo) => todo.id === action.payload);
+      if (index > -1) {
+        state.splice(index, 1);
+      }
+    },
+    modifyTodo(state, action: PayloadAction<Partial<Todo> & {id: number}>) {
+      //Partial이지만 id는 꼭 필요함
+      const index = state.findIndex(
+        (todo: Todo) => todo.id === action.payload.id,
+      );
+      if (index > -1) {
+        state[index] = {
+          ...state[index],
+          ...action.payload,
+        };
+      } else {
+        throw new Error("Todo doesn't exist");
+      }
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(clearTodo.fulfilled, (state, action) => {
+      const {dispatch, updatedFields} = action.payload;
+      dispatch(modifyTodo(updatedFields));
+    });
+  },
+});
+
+export const {addTodo, removeTodoById, modifyTodo} = todosSlice.actions;
+
+export default todosSlice.reducer;
